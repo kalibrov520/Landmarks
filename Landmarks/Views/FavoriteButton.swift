3ca6639e778c8a05f25b2c5b147c3f8bfa80e34c
IlyaMac <ilya.kalibrov@jetbrains.com>
2021-04-27 18:07:52 +0300

    FavoriteButton

---
+++ Landmarks/Views/FavoriteButton.swift
@@ -0,0 +1,22 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by Ilya Kalibrov on 4/27/21.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
