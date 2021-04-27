e266509f3d6bc21a641314a9f34d5a71d2637d64
KIlya <kalibrov520@gmail.com>
2021-04-27 18:25:11 +0300

    FB

--- Landmarks/Views/FavoriteButton.swift
+++ Landmarks/Views/FavoriteButton.swift
@@ -15,7 +15,6 @@ struct FavoriteButton: View {
             Image(systemName: isSet ? "star.fill" : "star")
                 .foregroundColor(isSet ? .yellow : .gray)
         }
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
